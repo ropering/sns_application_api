3920a96e341d1b92349aa5e92f17a5108d574f51
ropering <bonoboss1028@gmail.com>
2022-06-06 19:10:03 +0900

    [feat] model 설정

--- feed/models.py
+++ feed/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from account.models import User
 
-# Create your models here.
+
+class Feed(models.Model):
+    id = models.AutoField(primary_key=True, null=False, blank=False)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+    like_count = models.PositiveIntegerField(default=0)  # 좋아요 개수를 표시하기 위해 사용 (정수형, 0 이상)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    id = models.AutoField(primary_key=True, null=False, blank=False)
+    feed = models.ForeignKey(Feed, null=True, blank=True, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    like_count = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.comment

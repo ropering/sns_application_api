1b3f0a173afa2484f028cdb4b67422eeea18f7c7
ropering <bonoboss1028@gmail.com>
2022-06-06 19:14:19 +0900

    [feat] views 설정

--- account/views.py
+++ account/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.authentication import BasicAuthentication, SessionAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from account.models import User
+from account.serializers import UserSerializer, ProfileSerializer
+
+
+# 회원 가입
+class UserCreate(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+# 프로필
+class Profile(APIView):
+    authentication_classes = [TokenAuthentication, SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    # 프로필 조회
+    def get(self, request):
+        profile = User.objects.get(nickname=request.user)
+        serializer = ProfileSerializer(profile)
+        return Response(serializer.data)
